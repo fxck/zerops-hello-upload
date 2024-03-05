253bee84c1f35dbdb798d0b1072aef19779f1760
Aleš <foxxcz@gmail.com>
2024-03-05 16:48:46 +0100

    nx

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -9,6 +9,9 @@ import { toSignal } from '@angular/core/rxjs-interop';
   template: `
     <h2>Hello</h2>
 
+    <input type="file" (change)="onFileSelected($event)">
+    <button (click)="upload()">Upload</button>
+
     {{ dataSignal() | json }}
   `,
   styles: ``,
@@ -18,8 +21,27 @@ import { toSignal } from '@angular/core/rxjs-interop';
 })
 export class AppComponent {
   #http = inject(HttpClient);
-
-  #data$ = this.#http.get('https://api-14-3000.prg1.zerops.app/');
-
+  #data$ = this.#http.get('https://api-14-3000.prg1.zerops.app');
   dataSignal = toSignal(this.#data$);
+
+  selectedFile!: File | null;
+
+  onFileSelected(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.selectedFile = target.files ? target.files[0] : null;
+  }
+
+  upload() {
+    if (!this.selectedFile) { return; }
+    const formData = new FormData();
+
+    formData.append('file', this.selectedFile, this.selectedFile.name);
+    formData.append('target', 'OBJECT_STORAGE');
+
+    this.#http.post('https://api-14-3000.prg1.zerops.app/upload', formData).subscribe({
+      next: (response) => console.log('Upload successful', response),
+      error: (error) => console.error('Upload failed', error)
+    });
+
+  }
 }
