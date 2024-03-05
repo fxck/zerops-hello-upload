1bebd0379f3b80ba8752dcff0802420d218147c2
Aleš <foxxcz@gmail.com>
2024-03-05 16:20:42 +0100

    nx

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
   selector: 'zps-root',
   template: `
     <h2>Hello</h2>
+
+    {{ dataSignal() | json }}
   `,
-  styles: ``
+  styles: ``,
+  imports: [
+    JsonPipe
+  ]
 })
 export class AppComponent {
+  #http = inject(HttpClient);
+
+  #data$ = this.#http.get('https://api-14-3000.prg1.zerops.app/');
+
+  dataSignal = toSignal(this.#data$);
 }
