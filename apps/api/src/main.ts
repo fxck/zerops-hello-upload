f3e1a7a14bf65b792f678553927b6ecf12f84595
Aleš <foxxcz@gmail.com>
2024-03-06 01:04:55 +0100

    disk uplaod

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,16 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  // Serve static files from /var/www/data under the /static path
+  app.use('/static', express.static(join(__dirname, '..', 'data')));
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
